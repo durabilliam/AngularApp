1d523a1cb9ae0ac356ec42fefa5341d406a44bd1
William Perry <durabilliam@gmail.com>
2021-09-08 09:53:59 -0230

    add initial Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { FormlyFieldConfig } from '@ngx-formly/core';
 @Component({
   selector: 'app-root',
   template: `
+  <div class="container">
+  <h1>{{title}}</h1>
+  <p> This is an Example form using Angular,Formly,Material,Bottstrap</p>
+
   <form [formGroup]="form" (ngSubmit)="onSubmit()">
   <formly-form [form]="form" [fields]="fields" [model]="model">
   </formly-form>
@@ -14,18 +18,86 @@ import { FormlyFieldConfig } from '@ngx-formly/core';
 })
 
 export class AppComponent {
+  title = 'Angular NGX-Formly'
   form= new FormGroup({});
-  model = { email: 'email@gmail.com' };
+  model = {};
   fields: FormlyFieldConfig[] = [
+    {
+      key: 'name',
+      type: 'input',
+      templateOptions: {
+        label: 'Name',
+        placeholder: 'Enter name',
+      }
+    },
     {
       key: 'email',
       type: 'input',
       templateOptions: {
-        label: 'Email address',
+              type: 'email',
+        label: 'Email',
         placeholder: 'Enter email',
-        required: true,
       }
-    }
+    },
+    {
+      key: 'amount',
+      type: 'input',
+      templateOptions: {
+              type: 'number',
+        label: 'Amount',
+        placeholder: 'Enter amount',
+      }
+    },
+    {
+      key: 'date_of_birth',
+      type: 'datepicker',
+          templateOptions: {
+        label: 'Datepicker',
+        placeholder: 'Placeholder',
+        description: 'Description',
+        required: true,
+      },
+    },
+    {
+      key: 'terms',
+          type: 'checkbox',
+      templateOptions: {
+        label: 'Accept terms',
+        description: 'Please accept our terms',
+        required: true,
+      },
+    },
+    {
+      key: 'terms',
+      type: 'toggle',
+      templateOptions: {
+        label: 'Accept terms',
+        description: 'Please accept our terms',
+        required: true,
+      },
+    },
+    {
+      key: 'description',
+      type: 'textarea',
+      templateOptions: {
+        label: 'Description',
+        placeholder: 'Enter description',
+      }
+    },
+    {
+      key: 'gender',
+      type: 'radio',
+      templateOptions: {
+        label: 'Gender',
+        placeholder: 'Placeholder',
+        description: 'Fill in your gender',
+        options: [
+          { value: 1, label: 'Male' },
+          { value: 2, label: 'Female' },
+          { value: 3, label: 'I don\'t want to share that' },
+        ],
+      },
+    },
   ];
 
   onSubmit() {
