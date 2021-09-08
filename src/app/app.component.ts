e9168fa432c505a30a68ac75fda76266b9622deb
William Perry <durabilliam@gmail.com>
2021-09-08 09:41:21 -0230

    inital setup with Formly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <form [formGroup]="form" (ngSubmit)="onSubmit()">
+  <formly-form [form]="form" [fields]="fields" [model]="model">
+  </formly-form>
+  <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+  `,
 })
+
 export class AppComponent {
-  title = 'AngularApp';
+  form= new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      templateOptions: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    }
+  ];
+
+  onSubmit() {
+    console.log(this.model)
+  }
 }
